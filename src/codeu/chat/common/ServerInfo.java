255f5b4a64db2ae3eb48806e3d3a514f2d58ea6f
arshana <arshanajain@gmail.com>
2017-06-04 19:39:21 -0700

    Added some comments

--- src/codeu/chat/common/ServerInfo.java
+++ src/codeu/chat/common/ServerInfo.java
@@ -7,12 +7,15 @@ import java.io.IOException;
 /**
  * Created by arshana on 5/25/17.
  * Created during Version Check technical activity
+ * Holds Server's information
+ * Used by both Client and Server
  */
 public final class ServerInfo {
     private final static String SERVER_VERSION = "1.0.0";
     public final Uuid version;
     public final Time startTime;
-
+    
+    //constructor
     public ServerInfo(){
         this.startTime = Time.now();
         Uuid version1;
@@ -24,11 +27,13 @@ public final class ServerInfo {
         }
         this.version = version1;
     }
-
+    
+    //constructor added during Version Check activity
     public ServerInfo(Uuid version) {
         this.version = version;
     }
   
+    //constructor added during UpTime activity
     public ServerInfo(Time startTime) {
       this.startTime = startTime;
     }
