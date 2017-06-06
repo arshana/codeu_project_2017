c011439d36c8a9fe0f1f5b3035838508e99c1b1d
arshana <arshanajain@gmail.com>
2017-06-05 20:28:10 -0700

    Updates after Code Review

--- src/codeu/chat/common/ServerInfo.java
+++ src/codeu/chat/common/ServerInfo.java
@@ -1,8 +1,9 @@
 package codeu.chat.common;
 
-import codeu.chat.util.Uuid;
-import codeu.chat.util.Time;
 import java.io.IOException;
+
+import codeu.chat.util.Time;
+import codeu.chat.util.Uuid;
 
 /**
  * Created by arshana on 5/25/17.
@@ -11,30 +12,30 @@ import java.io.IOException;
  * Used by both Client and Server
  */
 public final class ServerInfo {
-    private final static String SERVER_VERSION = "1.0.0";
-    public final Uuid version;
-    public final Time startTime;
+  private final static String SERVER_VERSION = "1.0.0";
+  public final Uuid version;
+  public final Time startTime;
     
-    //constructor
-    public ServerInfo(){
-        this.startTime = Time.now();
-        Uuid version1;
-        try {
-            version1 = Uuid.parse(SERVER_VERSION);
-        } catch (IOException e) {
-            e.printStackTrace();
-            version1 = null;
-        }
-        this.version = version1;
+  //constructor
+  public ServerInfo(){
+    this.startTime = Time.now();
+    Uuid version1;
+    try {
+      version1 = Uuid.parse(SERVER_VERSION);
+    } catch (IOException e) {
+      e.printStackTrace();
+      version1 = null;
     }
+      this.version = version1;
+  }
     
-    //constructor added during Version Check activity
-    public ServerInfo(Uuid version) {
-        this.version = version;
-    }
+  //constructor added during Version Check activity
+  public ServerInfo(Uuid version) {
+    this.version = version;
+  }
   
-    //constructor added during UpTime activity
-    public ServerInfo(Time startTime) {
-      this.startTime = startTime;
-    }
+  //constructor added during UpTime activity
+  public ServerInfo(Time startTime) {
+    this.startTime = startTime;
+  }
 }
