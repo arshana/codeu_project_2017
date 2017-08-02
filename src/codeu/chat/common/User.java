7f2caf0862ef913b9b6a2022c665f6601e3d8a64
arshana <arshanajain@gmail.com>
2017-08-01 21:04:18 -0700

    Update User.java

--- src/codeu/chat/common/User.java
+++ src/codeu/chat/common/User.java
@@ -55,14 +55,12 @@ public final class User {
   public final Uuid id;
   public final String name;
   public final Time creation;
-  public final Set<Interest> interests;
 
   public User(Uuid id, String name, Time creation) {
 
     this.id = id;
     this.name = name;
     this.creation = creation;
-    this.interests = new HashSet<Interest>();
 
   }
 }
